27cb1967b9d21e9834f3f84684cebf6e15ef81f5
ryanherman1 <ryan.herman250@gmail.com>
2025-04-18 10:23:23 +1000

    debug logs added to check scraper working

--- backend/src/database/database.service.ts
+++ backend/src/database/database.service.ts
@@ -4,6 +4,8 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
+        console.log('🚀 Connecting to the database...');
         await this.$connect();
+        console.log('✅ Connected to the database!');
     }
 }
